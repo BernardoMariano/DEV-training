14d36f391fe0e139b66186729508eef84396fc51
Bernardo Mariano <bmariano@hugeinc.com>
2015-04-20 14:14:32 -0300

    Menu React

---
+++ menu_react/assets/js/main.js
@@ -0,0 +1,59 @@
+// Renders <li>
+var MenuItem = React.createClass({
+    getInitialState: function() {
+        var item = this.props.data;
+        return {
+            text: item.text,
+            subMenu: item.menu ? <MenuList data={item.menu} /> : null
+        }
+    },
+    render: function() {
+        return (
+            <li>
+                {this.state.text}
+                {this.state.subMenu}
+            </li>
+        );
+    }
+});
+
+
+// Renders <ul>
+var MenuList = React.createClass({
+    componentDidMount: function() {
+        if (!this.props.url) {
+            this.setState({data: this.props.data});
+        } else {
+            $.ajax({
+                url: this.props.url,
+                dataType: 'json',
+                success: function(response) {
+                    this.setState({data: response.menu});
+                }.bind(this)
+            });
+        }
+    },
+    getInitialState: function() {
+        return {
+            data: []
+        }
+    },
+    render: function() {
+        var items = [];
+        this.state.data.map(function(item, index) {
+            items.push(
+                <MenuItem data={item} key={index} />
+            );
+        });
+        return (
+            <ul className='menuList'>
+                {items}
+            </ul>
+        );
+    }
+});
+
+React.render(
+  <MenuList url='data.json' />,
+  document.getElementById('menu-app')
+);
